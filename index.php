1d5f17181b7fe6193f5d7a3fadabc197de072e62
Abdulrahman Fawzy <AbdulrahmanFawzy999@gmail.com>
2022-08-02 12:46:20 +0200

    add task

--- index.php
+++ index.php
@@ -1,12 +1,21 @@
+<?php
+$number_of_zoom = 11;
+$people = array("Ahmed","Mohamed","Adel");
+echo "number of people on Google Meeting = $number_of_zoom and the first person on our meeting list is $people[0]";
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="assets/index.css"/>
+    <title>Web Skills</title>
 </head>
 <body>
+
+
     
+<script src="assets/index.js"></script>
 </body>
 </html>
